83a89a386af90e6a3eb8ccfbf4e4aecb9e894bc4
Ryanokunoya <yunchez3@uci.edu>
2020-11-22 19:01:08 -0800

    Updates

---
+++ precache-manifest.07ca0e198522f2c04b6885099cf00f07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6be87942276f410e5b",
+    "url": "/to-do-list/static/css/main.9d37f864.chunk.css"
+  },
+  {
+    "revision": "0b6be87942276f410e5b",
+    "url": "/to-do-list/static/js/main.0b6be879.chunk.js"
+  },
+  {
+    "revision": "7706b1839108aa26e3ff",
+    "url": "/to-do-list/static/js/1.7706b183.chunk.js"
+  },
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "4d9df95814af7c5d24cf4141ee931694",
+    "url": "/to-do-list/index.html"
+  }
+];
